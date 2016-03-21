c1880912586c7539e542c733616c0909a615ab84
Ronando Long <ronando.long@gmail.com>
2016-03-21 14:04:24 -0700

    functioning For loop.

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,32 @@ $(function() {
   $('form').submit(function(event) {
     event.preventDefault();
 
-    var vowels = ['a', 'e', 'i', 'o', 'u'];
-    var sentence = ($('.sentence').val()).split(); // is an array
-    var newSentence = [];
+    var vowels = 'aeiou';  // Can traverse strings like arrays
+    var sentence = $('.sentence').val(); // Can traverse strings like arrays
+    var newSentence = sentence;  // not necessary, but I like it
 
-    for (var i = 0; i < vowels.length; i++) {  // loop through vowels
-      for (var j = 0; j < sentence.length; j++) { // loop through string
-        if (sentence.indexOf(vowels[i]) === -1) {
-          newSentence.push(sentence[j]);
-        } else {
-          newSentence.push('-');
-        }
-        console.log(newSentence);
-      }  // for loop - nested
-    } // for loop - outer
+    for (var i = 0; i < newSentence.length; i++) {
+      if (vowels.indexOf(newSentence[i]) > -1) {
+        newSentence = newSentence.replace(newSentence[i], '-');
+      }
+    }
+    console.log(newSentence);
+
+    //  Nested for loops, old and klunky!
+    // Need to loop through string first, then vowels second!
+    // for (var j = 0; j < newSentence.length; j++) { // loop through string
+    //   for (var i = 0; i < vowels.length; i++) {  // loop through vowels
+    //     if (newSentence[j].indexOf(vowels[i]) !== -1) {
+    //        newSentence[j].replace(vowels[i], '-');
+    //
+    //     } else {
+    //
+    //     }
+    //
+    //   }  // for loop
+    // } // for loop
+
+
 
 
   });
